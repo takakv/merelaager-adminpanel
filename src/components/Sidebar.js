95411ff9bb605ddda29f2e6f46cd85accf22be34
Taka <71265675+takakv@users.noreply.github.com>
2021-07-04 19:57:11 +0300

    Hide sensitive pages from normal users

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -7,57 +7,103 @@ import { setTitle } from "../features/pageTitle/pageTitleSlice";
 // close the navigation menu on mobile.
 const Navigation = () => {
   const dispatch = useDispatch();
+  const credentials = localStorage.getItem("credentials");
+  const role = JSON.parse(credentials).user.role;
   return (
     <nav className="c-admin-nav">
       <ul className="u-list-blank">
         <li className="last">
-          <Link className="_link" to="/" onClick={() => dispatch(setTitle("Kambüüs"))}>
+          <Link
+            className="_link"
+            to="/"
+            onClick={() => dispatch(setTitle("Kambüüs"))}
+          >
             Kambüüs
           </Link>
         </li>
+        {role === "op" ? (
+          ""
+        ) : (
+          <li>
+            <Link
+              className="_link"
+              to="/lapsed/"
+              onClick={() => dispatch(setTitle("Lapsed"))}
+            >
+              Lapsed
+            </Link>
+          </li>
+        )}
         <li>
-          <Link className="_link" to="/lapsed/" onClick={() => dispatch(setTitle("Lapsed"))}>
-            Lapsed
-          </Link>
-        </li>
-        <li>
-          <Link className="_link" to="/telgid/" onClick={() => dispatch(setTitle("Telgid"))}>
+          <Link
+            className="_link"
+            to="/telgid/"
+            onClick={() => dispatch(setTitle("Telgid"))}
+          >
             Telgid
           </Link>
         </li>
         <li className="last">
-          <Link className="_link" to="/meeskonnad/" onClick={() => dispatch(setTitle("Meeskonnad"))}>
+          <Link
+            className="_link"
+            to="/meeskonnad/"
+            onClick={() => dispatch(setTitle("Meeskonnad"))}
+          >
             Meeskonnad
           </Link>
         </li>
-        <li>
-          <Link className="_link"
-                to="/nimekiri/"
-                onClick={() => dispatch(setTitle("Nimekiri"))}
-          >
-            Nimekiri
-          </Link>
-        </li>
-        <li>
-          <Link className="_link"
-                to="/meil/"
-                onClick={() => dispatch(setTitle("Meil"))}>
-            Meil
-          </Link>
-        </li>
-        <li>
-          <Link className="_link"
-                to="/arvegeneraator/"
-                onClick={() => dispatch(setTitle("Arvegeneraator"))}
-          >
-            Arvegeneraator
-          </Link>
-        </li>
-        <li>
-          <Link className="_link" to="/sargid/" onClick={() => dispatch(setTitle("Särgid"))}>
-            Särgid
-          </Link>
-        </li>
+        {role === "op" ? (
+          ""
+        ) : (
+          <li>
+            <Link
+              className="_link"
+              to="/nimekiri/"
+              onClick={() => dispatch(setTitle("Nimekiri"))}
+            >
+              Nimekiri
+            </Link>
+          </li>
+        )}
+        {role === "op" ? (
+          ""
+        ) : (
+          <li>
+            <Link
+              className="_link"
+              to="/meil/"
+              onClick={() => dispatch(setTitle("Meil"))}
+            >
+              Meil
+            </Link>
+          </li>
+        )}
+        {role === "op" ? (
+          ""
+        ) : (
+          <li>
+            <Link
+              className="_link"
+              to="/arvegeneraator/"
+              onClick={() => dispatch(setTitle("Arvegeneraator"))}
+            >
+              Arvegeneraator
+            </Link>
+          </li>
+        )}
+        {role === "op" ? (
+          ""
+        ) : (
+          <li>
+            <Link
+              className="_link"
+              to="/sargid/"
+              onClick={() => dispatch(setTitle("Särgid"))}
+            >
+              Särgid
+            </Link>
+          </li>
+        )}
       </ul>
     </nav>
   );
@@ -79,7 +125,7 @@ const Sidebar = () => {
           width="50px"
         />
       </Link>
-      <Navigation/>
+      <Navigation />
     </div>
   );
 };
