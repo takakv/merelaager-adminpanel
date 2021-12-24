43899ba808477c200ccb6df4aaa36bb10837bbc5
Taka <71265675+takakv@users.noreply.github.com>
2021-12-24 18:12:46 +0100

    Fix login flow

--- src/app/App.js
+++ src/app/App.js
@@ -1,24 +1,8 @@
-import React, { useEffect } from "react";
-import { Route, Routes } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
+import React from "react";
 
 import Login from "../components/Login";
-import Sidebar from "../components/Sidebar";
-import PageTitle from "../components/PageTitle";
-import UserBox from "../components/UserBox";
-import RegistrationList from "../features/registrationList/RegistrationList";
-import BillGen from "../components/BillGen";
-import TentList from "../components/TentList";
-import TeamsPage from "../components/TeamList";
+import Root from "./Root";
 import useToken from "../useToken";
-import Shirts from "../features/thisrts/Tshirts";
-import ShiftInfo from "../features/camperInfo/CamperInfo";
-import Mailer from "../components/Mailer";
-import Hamburger from "../components/Hamburger";
-import MainPage from "../components/MainPage";
-import { setData } from "../features/userData/userDataSlice";
-import TimerList from "../components/TimerList";
-import { fetchInfo, getUserInfo } from "../features/userAuth/userAuthSlice";
 
 const apiURL =
   process.env.NODE_ENV === "development"
@@ -27,17 +11,6 @@ const apiURL =
 
 const App = () => {
   const { token, setToken } = useToken();
-
-  const dispatch = useDispatch();
-  const userInfo = useSelector(getUserInfo);
-  const userInfoStatus = useSelector((state) => state.userInfo.status);
-  const userInfoError = useSelector((state) => state.userInfo.error);
-
-  useEffect(() => {
-    if (userInfoStatus === "idle") dispatch(fetchInfo());
-  }, [userInfoStatus, dispatch]);
-
-  dispatch(setData(userInfo));
 
   if (!token) {
     return <Login setToken={setToken} />;
@@ -67,65 +40,7 @@ const App = () => {
     // alert("Autentimisega on probleeme. Palun anna Taanielile teada.");
   });
 
-  if (userInfoStatus === "succeeded") {
-    const { role } = userInfo;
-
-    return (
-      <div className="admin-page">
-        <Hamburger />
-        <Sidebar />
-        <PageTitle />
-        <UserBox />
-        <main role="main" className="c-content">
-          <Routes>
-            <Route exact path="/" element={<MainPage title="Kambüüs" />} />
-            {role === "op" ? (
-              ""
-            ) : (
-              <Route path="/lapsed/" element={<ShiftInfo title="Lapsed" />} />
-            )}
-            <Route
-              path="/meeskonnad/"
-              element={<TeamsPage title="Meeskonnad" />}
-            />
-            <Route path="/telgid/" element={<TentList title="Telgid" />} />
-            {role === "op" ? (
-              ""
-            ) : (
-              <Route path="/meil/" element={<Mailer title="Meil" />} />
-            )}
-            {role === "op" ? (
-              ""
-            ) : (
-              <Route
-                path="/nimekiri/"
-                element={<RegistrationList title="Nimekiri" />}
-              />
-            )}
-            {role === "op" ? (
-              ""
-            ) : (
-              <Route
-                path="/arvegeneraator/"
-                element={<BillGen title="Arvegeneraator" />}
-              />
-            )}
-            {role === "op" ? (
-              ""
-            ) : (
-              <Route path="/sargid/" element={<Shirts title="Särgid" />} />
-            )}
-            <Route path="/taimer/" element={<TimerList title="Taimer" />} />
-          </Routes>
-        </main>
-      </div>
-    );
-  }
-  if (userInfoStatus === "failed") {
-    window.location.reload();
-    return <p>{userInfoError}</p>;
-  }
-  return <p>Laen...</p>;
+  return <Root />;
 };
 
 export default App;
