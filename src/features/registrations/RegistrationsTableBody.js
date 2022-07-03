f0d894ea22bcf37e2c9365e0c13a90aca8b2c07d
Taka <71265675+takakv@users.noreply.github.com>
2022-07-03 12:07:45 +0300

    Calculate mean age

--- src/features/registrations/RegistrationsTableBody.js
+++ src/features/registrations/RegistrationsTableBody.js
@@ -4,15 +4,35 @@ import { useSelector } from "react-redux";
 import { selectGroupRegistrations } from "./registrationsSlice";
 import RegistrationsTableEntry from "./RegistrationsTableEntry";
 
+const calculateMeanAge = (registrations) => {
+  const now = new Date();
+  let ageSum = 0;
+
+  registrations.forEach((registration) => {
+    const {dob} = registration;
+    const birthDate = new Date(dob);
+    let age = now.getFullYear() - birthDate.getFullYear();
+    // Calculate the standing for June of the current year.
+    const mon = 5 - birthDate.getMonth();
+    if (mon < 0) age -= 1;
+    ageSum += age;
+  });
+
+  return Math.round(ageSum / registrations.length * 10) / 10;
+};
+
 const RegistrationsTableBody = ({ shiftNr, registered, gender }) => {
   let title;
-
-  if (registered) title = gender === "M" ? "Poisid" : "Tüdrukud";
-  else title = gender === "M" ? "Reserv poisid" : "Reserv tüdrukud";
 
   const registrations = useSelector((state) =>
     selectGroupRegistrations(state, shiftNr, registered, gender)
   );
+
+  if (registered) title = gender === "M" ? "Poisid" : "Tüdrukud";
+  else title = gender === "M" ? "Reserv poisid" : "Reserv tüdrukud";
+
+  const meanAge = calculateMeanAge(registrations);
+  title += ` | Keskmine vanus: ${meanAge}a`
 
   return (
     <tbody>
