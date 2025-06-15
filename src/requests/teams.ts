e3b56fe029bcc25691be9b341b14c3883de661f1
Taka <71265675+takakv@users.noreply.github.com>
2025-06-15 13:41:49 +0200

    Implement team creation

--- src/requests/teams.ts
+++ src/requests/teams.ts
@@ -1,9 +1,10 @@
-import { apiFetch } from '@/api/apiFetch.ts'
 import { StatusCodes } from 'http-status-codes'
 import { type Static, Type } from '@sinclair/typebox'
 import { queryOptions } from '@tanstack/react-query'
 
-export const TeamRecordSchema = Type.Object({
+import { apiFetch } from '@/api/apiFetch.ts'
+
+const TeamRecordSchema = Type.Object({
   id: Type.Integer(),
   shiftNr: Type.Integer(),
   name: Type.String(),
@@ -12,13 +13,51 @@ export const TeamRecordSchema = Type.Object({
   captainId: Type.Union([Type.Integer(), Type.Null()]),
 })
 
+const TeamCreationSchema = Type.Object({
+  shiftNr: Type.Integer(),
+  name: Type.String(),
+})
+
 export type TeamRecord = Static<typeof TeamRecordSchema>
+
+export type TeamCreationData = Static<typeof TeamCreationSchema>
 
 type ShiftTeamsAPISuccessResponse = {
   status: 'success'
   data: {
     teams: TeamRecord[]
   }
+}
+
+export const createShiftTeam = async (shiftNr: number, teamName: string) => {
+  const response = await apiFetch(`/teams`, {
+    method: 'POST',
+    mode: 'cors',
+    credentials: 'include',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      shiftNr,
+      name: teamName,
+    }),
+  })
+
+  if (response.status === StatusCodes.CREATED) return
+
+  const jsRes = await response.json()
+
+  if (!response.ok) {
+    switch (response.status) {
+      case StatusCodes.UNAUTHORIZED:
+        throw new Error('Ligipääsuks pead olema autenditud!')
+      case StatusCodes.UNPROCESSABLE_ENTITY:
+        throw new Error(jsRes.data.name)
+      default:
+        console.error(jsRes)
+        throw new Error('Ootamatu viga: rohkem infot konsoolis.')
+    }
+  }
+
+  throw new Error('Ootamatu vastus serverilt. Palun anna Taanielile teada.')
 }
 
 export const fetchShiftTeams = async (shiftNr: number) => {
--- src/routes/_auth/meeskonnad.tsx
+++ src/routes/_auth/meeskonnad.tsx
@@ -1,5 +1,12 @@
 import { createFileRoute } from '@tanstack/react-router'
-import { useSuspenseQuery } from '@tanstack/react-query'
+import {
+  useMutation,
+  useQueryClient,
+  useSuspenseQuery,
+} from '@tanstack/react-query'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
 
 import { getUserShift } from '@/utils.ts'
 
@@ -8,7 +15,9 @@ import {
   shiftRecordsFetchQueryOptions,
 } from '@/requests/shift-records.ts'
 import {
+  createShiftTeam,
   shiftTeamsFetchQueryOptions,
+  type TeamCreationData,
   type TeamRecord,
 } from '@/requests/teams.ts'
 
@@ -24,6 +33,15 @@ import {
   SelectTrigger,
   SelectValue,
 } from '@/components/ui/select.tsx'
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form.tsx'
+import { toast } from 'sonner'
 
 export const Route = createFileRoute('/_auth/meeskonnad')({
   component: RouteComponent,
@@ -33,6 +51,25 @@ export const Route = createFileRoute('/_auth/meeskonnad')({
     queryClient.ensureQueryData(shiftTeamsFetchQueryOptions(shiftNr))
   },
 })
+
+type TeamBoxProps = {
+  members: CamperRecord[]
+  team: TeamRecord
+}
+
+const TeamBox = ({ members, team }: TeamBoxProps) => {
+  return (
+    <div className="p-6 border rounded-md w-full md:w-56">
+      <div className="text-center">{team.name}</div>
+      <Separator className="my-4" />
+      <div className="flex flex-col gap-2">
+        {members.map((record) => (
+          <div key={record.id}>{record.childName}</div>
+        ))}
+      </div>
+    </div>
+  )
+}
 
 type TeamlessChildProps = {
   record: CamperRecord
@@ -62,6 +99,73 @@ const TeamlessChild = ({ record, teams }: TeamlessChildProps) => {
   )
 }
 
+const TeamCreateSchema = z.object({
+  teamName: z.string().min(1, {
+    message: 'Meeskonna nimi ei tohi olla tühi!',
+  }),
+})
+
+const CreateTeamCard = ({ shiftNr }: { shiftNr: number }) => {
+  const queryClient = useQueryClient()
+
+  const form = useForm<z.infer<typeof TeamCreateSchema>>({
+    resolver: zodResolver(TeamCreateSchema),
+    defaultValues: {
+      teamName: '',
+    },
+  })
+
+  const mutation = useMutation({
+    mutationFn: (createData: TeamCreationData) => {
+      return createShiftTeam(createData.shiftNr, createData.name)
+    },
+    onSuccess: async () => {
+      await queryClient.invalidateQueries({
+        queryKey: ['teams', shiftNr],
+        refetchType: 'active',
+      })
+      form.setValue('teamName', '')
+    },
+    onError: (error: Error) => {
+      toast.error('Viga meeskonna loomisel!', {
+        description: error.message,
+      })
+    },
+  })
+
+  const onSubmit = async (values: z.infer<typeof TeamCreateSchema>) => {
+    const { teamName } = values
+    mutation.mutate({ shiftNr, name: teamName })
+  }
+
+  return (
+    <div className="p-6 border rounded-md flex gap-4 flex-col">
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="flex flex-col md:flex-row gap-4 md:items-end"
+        >
+          <FormField
+            control={form.control}
+            name="teamName"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Meeskonna nimi</FormLabel>
+                <FormControl>
+                  <Input placeholder="Kasvatajad" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Loo meeskond</Button>
+        </form>
+      </Form>
+      <div>Meeskonna nime saab hiljem muuta.</div>
+    </div>
+  )
+}
+
 function RouteComponent() {
   const shiftNr = getUserShift()
 
@@ -79,7 +183,7 @@ function RouteComponent() {
   teams.sort((a, b) => a.name.localeCompare(b.name))
 
   return (
-    <div className="px-6 flex flex-col gap-6 overflow-y-scroll h-[calc((100%-var(--spacing)*22))]">
+    <div className="px-6 pb-6 flex flex-col gap-6 overflow-y-scroll h-[calc((100%-var(--spacing)*16))]">
       {teamlessCampers.length > 0 && (
         <div className="p-6 border rounded-md">
           <div>Lapsed</div>
@@ -91,16 +195,11 @@ function RouteComponent() {
           </div>
         </div>
       )}
-      <div className="p-6 border rounded-md flex gap-4 flex-col">
-        <div className="flex flex-col md:flex-row gap-4">
-          <Input
-            type="string"
-            placeholder="Meeskonna nimi"
-            className="w-full md:max-w-64"
-          />
-          <Button>Loo meeskond</Button>
-        </div>
-        <div>Meeskonna nime saab hiljem muuta.</div>
+      <CreateTeamCard shiftNr={shiftNr} />
+      <div className="flex flex-wrap gap-6">
+        {teams.map((team) => (
+          <TeamBox key={team.id} team={team} members={records} />
+        ))}
       </div>
     </div>
   )
