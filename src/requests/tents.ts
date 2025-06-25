ca0ed3d555851545526f5967b4ee6698dfce777e
Taka <71265675+takakv@users.noreply.github.com>
2025-06-25 15:28:52 +0300

    Implement tent score display

--- src/requests/tents.ts
+++ src/requests/tents.ts
@@ -7,6 +7,7 @@ import { apiFetch } from '@/api/apiFetch.ts'
 export const TentScoreSchema = Type.Object({
   score: Type.Number(),
   createdAt: Type.String(),
+  tentNr: Type.Number(),
 })
 
 export const TentInfoSchema = Type.Object({
@@ -21,6 +22,13 @@ export type TentScore = Static<typeof TentScoreSchema>
 type TentAPISuccessResponse = {
   status: 'success'
   data: TentInfo
+}
+
+type TentsAPISuccessResponse = {
+  status: 'success'
+  data: {
+    scores: TentScore[]
+  }
 }
 
 export const fetchTentInfo = async (shiftNr: number, tentNr: number) => {
@@ -45,6 +53,30 @@ export const fetchTentInfo = async (shiftNr: number, tentNr: number) => {
   }
 
   return (jsRes as TentAPISuccessResponse).data
+}
+
+export const fetchTentScores = async (shiftNr: number) => {
+  const response = await apiFetch(`/shifts/${shiftNr}/tents`, {
+    method: 'GET',
+    mode: 'cors',
+    credentials: 'include',
+  })
+
+  const jsRes = await response.json()
+
+  if (!response.ok) {
+    switch (response.status) {
+      case StatusCodes.UNAUTHORIZED:
+        throw new Error('Ligipääsuks pead olema autenditud!')
+      case StatusCodes.FORBIDDEN:
+        throw new Error(jsRes.data.permissions)
+      default:
+        console.error(jsRes)
+        throw new Error('Ootamatu viga: rohkem infot konsoolis.')
+    }
+  }
+
+  if (response.ok) return (jsRes as TentsAPISuccessResponse).data.scores
 }
 
 export const addTentScore = async (
@@ -78,4 +110,10 @@ export const shiftTentFetchQueryOptions = (shiftNr: number, tentNr: number) =>
   queryOptions({
     queryKey: ['tent', shiftNr, tentNr],
     queryFn: () => fetchTentInfo(shiftNr, tentNr),
+  })
+
+export const shiftTentsFetchQueryOptions = (shiftNr: number) =>
+  queryOptions({
+    queryKey: ['tents', shiftNr],
+    queryFn: () => fetchTentScores(shiftNr),
   })
--- src/routes/_auth/hinded.tsx
+++ src/routes/_auth/hinded.tsx
@@ -1,9 +1,107 @@
 import { createFileRoute } from '@tanstack/react-router'
 
+import { getUserShift } from '@/utils.ts'
+
+import {
+  shiftTentsFetchQueryOptions,
+  type TentScore,
+} from '@/requests/tents.ts'
+import { useSuspenseQuery } from '@tanstack/react-query'
+import { DataTable } from '@/components/data-table.tsx'
+import type { ColumnDef } from '@tanstack/table-core'
+
 export const Route = createFileRoute('/_auth/hinded')({
   component: RouteComponent,
+  loader: ({ context: { queryClient } }) => {
+    queryClient.ensureQueryData(shiftTentsFetchQueryOptions(getUserShift()))
+  },
 })
 
+const scoreColumns: ColumnDef<TentScore[]>[] = [
+  1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+].map((tentNr) => {
+  return {
+    header: `${tentNr}`,
+    cell: ({ row }) => {
+      let relevantEntry = Array.from(row.original.values()).find(
+        (e) => e.tentNr === tentNr,
+      )
+      if (relevantEntry) return relevantEntry.score
+      return '-'
+    },
+  }
+})
+
+const scoreTableColumns: ColumnDef<TentScore[]>[] = [
+  {
+    header: 'Kuupäev',
+    cell: ({ row }) => {
+      return Array.from(row.original.values())[0].createdAt
+    },
+  },
+  ...scoreColumns,
+]
+
 function RouteComponent() {
-  return <div>Töö käib</div>
+  const shiftNr = getUserShift()
+
+  const { data: scores } = useSuspenseQuery(
+    shiftTentsFetchQueryOptions(shiftNr),
+  )
+
+  const dateScores = new Map<string, TentScore[]>()
+
+  // Split scores by date.
+  scores?.forEach((score) => {
+    const dateString = new Date(score.createdAt).toLocaleDateString('et')
+    const entries = dateScores.get(dateString)
+    if (entries === undefined) {
+      dateScores.set(dateString, [score])
+      return
+    }
+    entries.push(score)
+  })
+
+  // Keep only a single score per tent per calendar day.
+  // Average multiple values for the same day.
+  const uniqueDateScores = new Map<string, TentScore[]>()
+  dateScores.forEach((tentInfo, dateString) => {
+    if (tentInfo.length === 0) {
+      uniqueDateScores.set(dateString, [])
+      return
+    }
+
+    const dateScores: TentScore[] = []
+    for (let i = 1; i <= 10; ++i) {
+      let totalCount = 0
+      let totalScore = 0
+      tentInfo.forEach((tent) => {
+        if (tent.tentNr === i) {
+          totalCount += 1
+          totalScore += tent.score
+        }
+      })
+
+      if (totalCount > 0) {
+        dateScores.push({
+          score: totalScore / totalCount,
+          tentNr: i,
+          createdAt: dateString,
+        })
+      }
+    }
+
+    uniqueDateScores.set(dateString, dateScores)
+  })
+
+  console.log(Array.from(uniqueDateScores.values()))
+
+  return (
+    <div className="px-6 pb-6 flex flex-col gap-6 overflow-y-scroll h-[calc((100%-var(--spacing)*16))]">
+      <DataTable
+        columns={scoreTableColumns}
+        data={Array.from(uniqueDateScores.values())}
+      />
+    </div>
+  )
 }
